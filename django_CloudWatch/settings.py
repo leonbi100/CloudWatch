14bfa4b3185d4a39a21865dd0106c9ec89261d39
Leon Bi <leon@Leons-MacBook-Pro-2.local>
2019-08-29 01:14:15 -0700

    white noise

--- django_CloudWatch/settings.py
+++ django_CloudWatch/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'django_celery_beat',
     'django_celery_results',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -74,6 +75,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'django_CloudWatch.urls'
@@ -150,6 +152,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
